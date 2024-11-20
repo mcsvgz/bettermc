c93cb5aa3f7ce7107334149f78088b78535791ec
mcsvgz <endergrixxyt@gmail.com>
2024-11-20 21:35:44 +0000

    Pending changes exported from your codespace

--- server.py
+++ server.py
@@ -1,29 +1,92 @@
-# Versiones disponibles
-# forge, mohist, fabric, vanilla, paper
-
-# Puedes instalar mohist después de instalar forge desde el menú de gestionar
-# Puedes instalar paper después de instalar vanilla desde el menú de gestionar
-# Puedes instalar purpur después de instalar fabric desde el menú de gestionar
-
-# Regiones de ngrok
-# Código          Lugar
-#-----------      ---------------------------
-# ap	          Asia / Pacífico (Singapore)
-# au		      Australia (Sydney)
-# eu		      Europa (Frankfurt)
-# in		      India (Mumbai)
-# jp		      Japón (Tokyo)
-# sa		      Sudamérica (São Paulo)
-# us		      Estados unidos (Ohio)
-# us-cal-1	      Estados unidos (California)
-
-
-
-
-
-
-
-
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# aaaw
+# gvvd
+# jdjd
+# ags
+# sa
+# aa
+# d
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# 
+# 
+# 
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# 
+# 
+# 
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# 
+# 
+# 
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# 
+# 
+# 
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# 
+# 
+# 
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# 
+# 
+# 
+# ESCRIBIR ACA UNA LETRA PARA MANTENER EL SERVER PRENDIDO POR MAS TIEMPO
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
 
 
 
